42ed9905d20ffc665b955ae9a70bc31259ee1ecb
SUBO3 <kevin9700387@gmail.com>
2024-12-27 18:13:28 +0800

    basic alarm
    基本功能

---
+++ app/src/main/java/ntutee/team3/JavaFinalProject/Alarm.java
@@ -0,0 +1,54 @@
+package ntutee.team3.JavaFinalProject;
+
+public class Alarm {
+    private int hour;
+    private int minute;
+    private boolean[] days; // 一周的天數
+
+    public Alarm(int hour, int minute, boolean[] days) {
+        this.hour = hour;
+        this.minute = minute;
+        this.days = days;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public boolean[] getDays() {
+        return days;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setDays(boolean[] days) {
+        if (days != null && days.length == 7) {
+            this.days = days.clone();
+        }
+    }
+
+
+    public String getFormattedDays() {
+        String[] dayNames = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        StringBuilder formattedDays = new StringBuilder();
+        for (int i = 0; i < days.length; i++) {
+            if (days[i]) {
+                if (formattedDays.length() > 0) {
+                    formattedDays.append(", ");
+                }
+                formattedDays.append(dayNames[i]);
+            }
+        }
+        return formattedDays.toString();
+    }
+}
